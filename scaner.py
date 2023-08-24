7b3eed9a5b45a463184755ddc52dd52de23f7c08
FOliwa <filip.oliwa@gmail.com>
2023-08-24 22:53:48 +0200

    Scaning multiple hosts and ports added

--- scaner.py
+++ scaner.py
@@ -8,7 +8,16 @@ class PortScaner:
     - A listening socket does just what its name suggests. It listens for connections from clients.
     - The client calls .connect() to establish a connection to the server and initiate the three-way handshake. 
     """
-    def start_scan(self, target_host="127.0.0.1", target_port=80):
+    def __init__(self, target_hosts=["127.0.0.1"], target_ports=[80]) -> None:
+        self.target_hosts = target_hosts
+        self.target_ports = target_ports
+
+    def run_scanner(self):
+        for host in self.target_hosts:
+            for port in self.target_ports:
+                self.scan(host, port)
+
+    def scan(self, target_host, target_port):
         try:
             # Create socket obj using with and close at the end
             with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client:
