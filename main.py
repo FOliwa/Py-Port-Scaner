734e6855ee5ece680bd8bbfde79d29826a3df2db
FOliwa <filip.oliwa@gmail.com>
2023-08-26 19:34:28 +0200

    Add user input sanitization

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
-from validator import InputValidator
+from typing import List
+
+from validator import InputSanitizer
+from scaner import PortScaner
 
 def main():
-    target_ip = get_target_ip_address()
-    get_the_port_range()
-    run_scaner()
+    ip_address = get_target_ip_address()
+    ports = get_target_ports()
+    scaner = PortScaner([ip_address], ports)
+    scaner.run_scanner()
     display_scanning_results()
 
 
@@ -14,31 +18,25 @@ def display_scanning_results():
     pass
 
 
-def run_scaner():
-    """ TODO:
-    Implement a port scanning logic that checks if a port is open or closed on the target host.
-    TIP: use Python's socket library to establish a connection to the target IP and port
-    """
-    pass
-
-
-def get_the_port_range():
+def get_target_ports() -> List[int]:
     while True:
-        target_ports = input("Enter ports to scan.\n 
-                          " - You can provide list of ports like: 80,444,6723"
-                          " - Or you can provide reange of ports to scan: 7-1000\n"
-                          "Set ports to scan: ")
-        if InputValidator.port_value_is_valid(target_ports):
-            return target_ports
+        ports_value = input("Enter ports to scan.\n"
+                            " - You can provide list of ports like: 80,444,6723\n"
+                            " - Or you can provide reange of ports to scan: 7-1000\n"
+                            "Set ports to scan: ")
+        sanitized_ports = InputSanitizer.sanitize_provided_ports_value(ports_value)
+        if sanitized_ports:
+            return sanitized_ports
         print(f"Provided ports value is invaild\n")
 
 
 def get_target_ip_address():
     while True:
-        target_ip = input("Enter target IP addres for scan: ")
-        if InputValidator.ip_address_is_valid(target_ip):
-            return target_ip
-        print(f"Wrong IP addres: {target_ip}\n")
+        provided_ip = input("Enter target IP addres for scan: ")
+        sanitize_ip = InputSanitizer.sanitize_provided_ip_value(provided_ip)
+        if sanitize_ip:
+            return sanitize_ip
+        print(f"Wrong IP address format: {provided_ip}\n")
 
 
 if __name__ == '__main__':
--- validator.py
+++ validator.py
@@ -1,12 +1,29 @@
-class InputValidator:
+import re
+
+
+class InputSanitizer:
 
     @staticmethod
-    def ip_address_is_valid(ip_addr: str) -> bool:
+    def sanitize_provided_ip_value(ip_addr: str):
         try:
-            return all(map(lambda x: x.isdigit(), ip_addr.split('.')))
+            ip_components = ip_addr.split('.')
+            if len(ip_components) == 4 and all(map(lambda x: x.isdigit(), ip_components)):
+                return ip_addr
         except Exception:
             return False
 
     @staticmethod
-    def port_value_is_valid(port: str) -> bool:
-        
+    def sanitize_provided_ports_value(ports_value: str):
+        range_pattern = r"(\d+)-(\d+)"
+        match = re.match(range_pattern, ports_value)
+        try:
+            if match:
+                start_value = int(match.group(1))
+                end_value = int(match.group(2))
+                return range(start_value, end_value) if start_value < end_value else False
+            else:
+                ports = ports_value.split(',')
+                if all([p.isdigit() for p in ports]):
+                    return (p for p in ports if p.isdigit())
+        except Exception:
+            return False
