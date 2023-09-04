87581fa0a3e9ccef04ca7f468cccef26622d4aa0
FOliwa <filip.oliwa@gmail.com>
2023-09-04 22:40:40 +0200

    Multi threading added to UDP scanning

--- scaner.py
+++ scaner.py
@@ -1,4 +1,6 @@
 import socket
+import threading
+from more_itertools import chunked
 
 
 class PortScaner:
@@ -9,27 +11,32 @@ class PortScaner:
     - The client calls .connect() to establish a connection to the server and initiate the three-way handshake. 
     """
     def __init__(self, target_hosts=["127.0.0.1"], target_ports: [list, range] = [80]) -> None:
+        self.debug = False
         self.target_hosts = target_hosts
         self.target_ports = target_ports
-        self.open_tcp_ports = []
-        self.open_upd_ports = []
+        self.results = {}
         self.scan_tcp = True
-        self.scan_udp = False
+        self.scan_udp = True
 
     def run_scanner(self):
-        # TODO: Find better way to scan it - split scaning to multiple threads
 
         for host in self.target_hosts:
+            self.results[host] = {"open_tcp_ports": [],
+                                  "open_udp_ports": []}
             if self.scan_tcp:
-                self.open_tcp_ports = [port for port in self.target_ports if self.tcp_port_open(host, port)]
-                # print(f"Open TCP ports for host: {host}:")
-                # print(f"{open_tcp_ports}")
+                for port in self.target_ports:
+                    self.tcp_port_open(host, port)
 
             if self.scan_udp:
-                self.open_upd_ports = [port for port in self.target_ports if self.udp_port_open(host, port)]
-                # print(f"Open UDP ports for host: {host}:")
-                # print(f"{open_udp_ports}")
+                threads = []
+                chunk_size = int(len(self.target_ports)/10)
+                for ports_chunk in chunked(self.target_ports, chunk_size):
+                    thread = threading.Thread(target=self.udp_port_open, args=(host, ports_chunk))
+                    thread.start()
+                    threads.append(thread)
 
+                for thread in threads:
+                    thread.join()
 
     def tcp_port_open(self, target_host, target_port):
         """ Scan TCP port by establish connection between targeted system on 
@@ -39,52 +46,63 @@ class PortScaner:
             # Try to establish connection
             with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as tcp_client:
                 tcp_client.connect((target_host, target_port))
-                print(f"Connection to {target_host}:{target_port} is established!")
-                return True
+                if self.debug:
+                    print(f"Connection to {target_host}:{target_port} is established!")
+                self.results["open_tcp_ports"].append(target_port)
         except socket.timeout:
-            pass
-            # print(f"Port {target_port} is closed (timeout)")
+            if self.debug:
+                print(f"Port {target_port} is closed (timeout)")
         except ConnectionRefusedError:
-            pass
-            # print(f"Port {target_port} is closed (connection refused)")
+            if self.debug:
+                print(f"Port {target_port} is closed (connection refused)")
         except Exception as e:
-            pass
-            # print(f"Connection to {target_host}:{target_port} failed: {e}")
-        return False
+            if self.debug:
+                print(f"Connection to {target_host}:{target_port} failed: {e}")
 
-    def udp_port_open(self, target_host, target_port):
+    def udp_port_open(self, target_host, target_ports):
         """ Scanning UDP ports required diffrent approach than scaning TCP ports becouse
         there is no "connection" to establish. Thats why I have to realy on recieiving some
         response from saning port of targeted system.
         """
+        print(f"Start Thread for ports: {target_ports}")
         with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as udp_client:
             # Timeout for reciving reponses
             # Scaning UDP take much more time then TCP
             udp_client.settimeout(1)
-            try:
-                udp_client.sendto(b"Hello!", (target_host, target_port))
-                response, _ = udp_client.recvfrom(1024)
-                print(f"UDP Port {target_port} is open")
-            except socket.timeout:
-                """
-                Analyzing response in UDP case is not so obvius like in TCP case
-                If you don't receive any response (timeout), the port's state is uncertain.
-                Some options is such case are:
-                - retry mechanism
-                - multiple scans
-                - scan commonly use ports (specific for knonw services)
-                - if port is TCP you dont need UDP scan on it then
-                """
-                print(f"Timeout: UDP Port {target_port} state is uncertain - most probably filtered or closed")
-            except Exception as e:
-                print(f"Error scaning UDP Port {target_port}: {e}")
-        return False
+            for port in target_ports:
+                try:
+                    udp_client.sendto(b"Hello!", (target_host, port))
+                    response, _ = udp_client.recvfrom(1024)
+                    if self.debug:
+                        print(f"UDP Port {port} is open")
+                    self.results["open_udp_ports"].append(port)
+                except socket.timeout:
+                    """
+                    Analyzing response in UDP case is not so obvius like in TCP case
+                    If you don't receive any response (timeout), the port's state is uncertain.
+                    Some options is such case are:
+                    - retry mechanism
+                    - multiple scans
+                    - scan commonly use ports (specific for knonw services)
+                    - if port is TCP you dont need UDP scan on it then
+                    """
+                    if self.debug:
+                        print(f"Timeout: UDP Port {port} state is uncertain - most probably filtered or closed")
+                except Exception as e:
+                    if self.debug:
+                        print(f"Error scaning UDP Port {port}: {e}")
 
     def display_scanning_results(self):
-        print(f"Open ports for host: {self.target_hosts}:")
-        if self.scan_tcp:
-            print(f"Open TCP ports for host: {self.open_tcp_ports}:")
-        if self.scan_udp:
-            print(f"Open TCP ports for host: {self.open_upd_ports}:")
-        if any([self.scan_tcp, self.scan_udp]):
-            print("Nothing to scann - select TCP or UDP port scaning.")
+        for host, ports in self.results.items():
+            print("_" * 40)
+            print(f"Open ports for host: {host}:")
+            print(type(ports))
+            print(ports)
+            if self.scan_tcp:
+                open_tcp_ports = ports['open_tcp_ports']
+                print(f"Open TCP ports:\n\t {open_tcp_ports}\n")
+            if self.scan_udp:
+                open_udp_ports = ports['open_udp_ports']
+                print(f"Open UDP ports:\n\t {open_udp_ports}\n")
+            if not any([self.scan_tcp, self.scan_udp]):
+                print("Nothing to scann - select TCP or UDP port scaning.\n")
